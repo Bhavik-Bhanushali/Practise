a822f647256b71e0a3702a5053ca91eb4262304b
Bhavik <bhanushalibhavik37@gmail.com>
2024-08-28 10:08:42 +0530

    add date.js, loops.js, math.js, set.js

---
+++ Practise/p9 Variables and Constants, Primitive Types and Dynamic Typing/loops.js
@@ -0,0 +1,50 @@
+// for loop
+// for(let i = 1; i < 11; i++){
+//     console.log(++i)
+//   }
+//   let i = 1;
+//   for(; ;){
+//     console.log(i)
+//     i++;
+//     if(i > 10) break
+//   }
+
+// for in loop
+// 
+// const arr = [1,2,3,4,5,6];
+// const obj = {
+//   firstName : "Bhavik",
+//   lastName : "Bhanushali",
+//   age : 20,
+//   contact : 9326542426,
+// };
+
+// for(let i in arr){
+//   console.log(i + " " + arr[i])
+// }
+// for(let i in obj){
+//   console.log(i + " " + obj[i])
+// }
+
+// forEach loop
+// const numbers = [45, 4, 9, 16, 25];
+
+// let sum = 0;
+// numbers.forEach(myFunction);
+// numbers.forEach(x => sum += x);
+// console.log(sum)
+// function myFunction(value,index, array) {
+//   console.log(index + " " +  value + " " + array[index])
+// }
+
+// for of loop
+// const numbers = [45, 4, 9, 16, 25];
+
+// for(let num of numbers){
+//   console.log(num)
+// }
+// const str = "Bhavik";
+// for(let c of str){
+//   console.log(c)
+// }
+
---
+++ Practise/p9 Variables and Constants, Primitive Types and Dynamic Typing/math.js
@@ -0,0 +1,16 @@
+// let num1 = 2.0000001, num2 = -2.50, num3 = 2.999999999;
+
+// console.log(`${Math.round(num1)} ${Math.round(num2)} ${Math.round(num3)}`);
+// console.log(`${Math.ceil(num1)} ${Math.ceil(num2)} ${Math.ceil(num3)}`)
+// console.log(`${Math.floor(num1)} ${Math.floor(num2)} ${Math.floor(num3)}`)
+// console.log(`${Math.sign(4)} ${Math.sign(0)} ${Math.sign(-4)}`)
+
+// console.log(Math.pow(8,2))
+// console.log(Math.sqrt(225))
+// console.log(`${Math.abs(4.7)} ${Math.abs(-4.7)}`)
+// console.log(Math.min(1,23,4,6,77,8,81,-1))
+// console.log(Math.max(1,23,4,6,77,8,81,-1))
+// console.log(Math.random())
+// console.log(Math.log(1) + " " + Math.log2(2) + " " + Math.log10(10))
+// console.log(Math.random() * 100 + 1)
+// console.log(parseInt(Math.random() *100 + 1))
