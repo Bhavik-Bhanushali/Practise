8352b5435f91e3e27b88780d67a312507f77a913
Bhavik <bhanushalibhavik37@gmail.com>
2024-08-28 12:41:20 +0530

    add map.js and map.html

---
+++ Practise/p9 Variables and Constants, Primitive Types and Dynamic Typing/map.js
@@ -0,0 +1,36 @@
+// const map1 = new Map([
+//   ["apple", 1000],
+//   ["banana", 750],
+//   ["cherry", 500]
+//   ]);
+  
+// console.log(map1);
+// console.log(map1.get("apple"));
+// console.log(map1.get("banana"));
+// map1.set("apple", 1200)
+// console.log(map1)
+// console.log(map1.get("apple"))
+// console.log(typeof map1)
+// console.log(map1 instanceof Map)
+// map1.set("pair", 300)
+// console.log(map1)
+// console.log(map1.size)
+// map1.delete("pair")
+// console.log(map1)
+// console.log(map1.get("pair"));
+// console.log(map1.has("apple"))
+// console.log(map1.has("pair"))
+// console.log(map1.keys())
+// console.log(map1.values())
+// console.log(map1.entries())
+// const mango = {name: "mango"}
+// map1.set(mango,2000)
+// console.log(map1)
+// console.log(map1.get(mango))
+// map1.clear()
+// console.log(map1)
+// map1.forEach(x => console.log(x))
+// map1.forEach(myFunction);
+// function myFunction(value, index){
+//   console.log(value + " " + index)
+// }
