3234c70b7b29b54a6c25df186dbb5176181aa4d9
Bhavik <bhanushalibhavik37@gmail.com>
2024-08-29 14:13:48 +0530

    add hoisting .js

---
+++ Practise/p9 Variables and Constants, Primitive Types and Dynamic Typing/hoisting.js
@@ -0,0 +1,21 @@
+// a = 10
+// console.log(a)
+// var a;
+
+// b = 12;
+// console.log(b)
+// let b; // referance Error
+
+// var c = 4;
+// var d = 6;
+// console.log(c + " " + d)
+
+// e = 43;
+// console.log(e + " " + f)
+// var e;
+// var f = 14 // only declararion go to top and hoist but initialization is is not go to top
+
+var g = 9
+var h;
+console.log(g + " " + h)
+h = 324;  // h is declare but not initialized
