2f5ac64700e35929a8505d0058f87e0d1e0b1a52
Bhavik <bhanushalibhavik37@gmail.com>
2024-08-30 11:46:25 +0530

    add folder p10 JS and add map_reduce_filter.js in it

---
+++ Practise/p10 JS/map_reduce_filter.js
@@ -0,0 +1,26 @@
+const arr = [1,2,3,4,5,6,7,8]
+
+// --------------------------------------------
+// map()
+// const func1 = (n) => n ** 2;
+// // const sqArr = arr.map(func1)   // Choise 1 method
+// const sqArr = arr.map(x => x ** 2)
+// console.log(arr)
+// console.log(sqArr)
+// console.log(arr)
+
+// // --------------------------------------------
+// // reduce()
+// const func2 = (a, b) => a * b;
+// // const result = arr.reduce(func2)  // Choise 1 method
+// const result = arr.reduce((a,b) => a * b)
+// console.log(result)
+// console.log(arr)
+
+// // --------------------------------------------
+// // filter()
+// const func3 = x => x % 2 === 0;
+// // const evenArr = arr.filter(func3);   // Choise 1 method
+// const evenArr = arr.filter(x => x % 2 === 0)
+// console.log(evenArr)
+// console.log(arr)
