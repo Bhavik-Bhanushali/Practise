02a887febec5e06e1e4674d83f119077853a1503
Bhavik <bhanushalibhavik37@gmail.com>
2024-09-02 14:23:23 +0530

    Add module1.js, module2.js and package.json files

---
+++ 2-9-2024/module1.js
@@ -0,0 +1,7 @@
+// const num = 100;
+const message = () => {
+    console.log("This is module1.js File");   
+}
+// export { num, message }
+export const num = 100;
+export default message;
---
+++ 2-9-2024/module2.js
@@ -0,0 +1,6 @@
+// import {num, message} from "./module1.js"
+import message,{num} from "./module1.js";
+
+console.log(num);
+
+message()
