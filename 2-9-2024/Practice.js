e89740935f04ff5249ad16a588be41c9f91aa8cd
Bhavik <bhanushalibhavik37@gmail.com>
2024-09-02 18:38:09 +0530

    Add practice.js

---
+++ 2-9-2024/Practice.js
@@ -0,0 +1,48 @@
+// Rotate Image
+// const arr = [[1,2,3],[4,5,6],[7,8,9]];
+// let count1 = 0;
+// let count2 = 2;
+
+// for(const temp of arr){
+//   console.log(temp)
+// }
+
+// // const arr90 = []
+// // for(let i = 0; i < 3; i++){
+// //   arr90[i] = [];
+// // }
+
+// // for(let i = 2; i >= 0; i--){
+// //   for(let j = 0; j < 3; j++){
+// //     arr90[j][count1] = arr[count2][j];
+// //   }
+// //   count1++;
+// //   count2--;
+// // }
+
+// // for(const temp of arr90){
+// //   console.log(temp)
+// // }
+
+// for(let i = 0; i < 3; i++){
+//   for(let j = 0; j <= i ; j++){
+//     if(i === j) continue;
+//     else{
+//       let temp = arr[i][j];
+//       arr[i][j] = arr[j][i]
+//       arr[j][i] = temp;
+//     }
+//   }
+// }
+
+// for(let i = 0; i < 3; i++){
+//   let temp = arr[i][0];
+//   arr[i][0] = arr[i][2];
+//   arr[i][2] = temp;
+// }
+
+// for(const temp of arr){
+//   console.log(temp)
+// }
+
+
